7ba510e5f166c504b919e90e6320331c8f75517d
feandrrrr <artyomg840@gmail.com>
2026-09-22 14:11:43 +0100

    Add deployment data model

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Deployment(BaseModel):
+    service_name: str
+    environment: str
+    version: str
+    status: str
+    owner: str
+    risk_level: str
 
 @app.get("/")
 def root():
